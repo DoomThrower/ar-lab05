150be66e245762e589c6e6b032b6c958bee6a83d
Henrar <aargh1@gmail.com>
2016-01-18 13:01:00 +0100

    added results from zeus run

--- run_zeus.sh
+++ run_zeus.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=12
 module load plgrid/apps/spark
 
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10.txt > out/g10.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100.txt > out/g100.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g1000.txt > out/g1000.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10000.txt > out/g10000.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100000.txt > out/g100000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10.txt > output/g10.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100.txt > output/g100.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g1000.txt > output/g1000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10000.txt > output/g10000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100000.txt > output/g100000.out
