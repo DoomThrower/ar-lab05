fc572f4cfee7b751f06dd4d038e2b6b3f5f87424
Henrar <aargh1@gmail.com>
2016-01-18 18:03:11 +0100

    fix to run zeus file

--- run_zeus.sh
+++ run_zeus.sh
@@ -2,8 +2,8 @@
 #PBS -l nodes=1:ppn=12
 module load plgrid/apps/spark
 
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10.txt > output/g10.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100.txt > output/g100.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g1000.txt > output/g1000.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10000.txt > output/g10000.out
-$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100000.txt > output/g100000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10.txt > /people/plgbkumor/ar-lab05/output/g10.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100.txt > /people/plgbkumor/ar-lab05/output/g100.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g1000.txt > /people/plgbkumor/ar-lab05/output/g1000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g10000.txt > /people/plgbkumor/ar-lab05/output/g10000.out
+$SPARK_HOME/bin/spark-submit --class SparkConnectedComponents --master local[*] $HOME/ar-lab05/jar/connectedcomponents_2.10-1.0.jar graphs/g100000.txt > /people/plgbkumor/ar-lab05/output/g100000.out
